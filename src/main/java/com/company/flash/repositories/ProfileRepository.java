925248293b51a34cf612ed512d34aac712655d13
mardsantana <mardsonsantana105@gmail.com>
2024-08-14 15:31:42 -0300

    implements method of the method of password

--- src/main/java/com/company/flash/repositories/ProfileRepository.java
+++ src/main/java/com/company/flash/repositories/ProfileRepository.java
@@ -4,8 +4,10 @@ import com.company.flash.domain.profile.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.UUID;
 
-public interface ProfileRepository extends JpaRepository<Profile, String> {
 
-    UserDetails findByUsername(String username);
+public interface ProfileRepository extends JpaRepository<Profile, UUID> {
+    Profile findByUsername(String username);
+
 }
